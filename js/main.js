79d66e9b6d360fe10be6f0ccec8f74ec191b5710
olegJS95 <Oleg.Holoshchapov@filiatix.com>
2023-05-30 20:35:48 +0300

    task in progress

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var $data = {};
-    $('#submit-button-registration').on("click", function(e) {
+    $('#submit-button-registration').on("click", function (e) {
         e.preventDefault()
-        $('#myForm').find ('input, textearea, select').each(function() {
+        $('#myForm').find('input, textearea, select').each(function () {
             $data[this.name] = $(this).val();
         });
-        alert("function works")
         console.log("$data")
         console.log($data)
+        $.post("http://localhost:1337/api/auth/local/register",
+            {
+                "username": $data.user-name,
+                "email": $data.email,
+                "password": $data.password
+            },
+            function (data, status) {
+                alert("Данные: " + data + "\nСостояние: " + status);
+            });
     })
 }); 
 
