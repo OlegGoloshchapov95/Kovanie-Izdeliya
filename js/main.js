901325fd53b6ba3ca31c72033b4d3700064ba45c
olegJS95 <Oleg.Holoshchapov@filiatix.com>
2023-05-31 20:59:40 +0300

    task in progress

--- js/main.js
+++ js/main.js
@@ -15,18 +15,10 @@ $(document).ready(function () {
             },
             function (data, status) {
                 console.log("Данные: " + JSON.stringify(data) + "\nСостояние: " + status);
-                alert("data.jwt")
-                alert(data.jwt)
                 sessionStorage.setItem("bearerTokenForUser", `Bearer ${data.jwt}`);
-                alert("$data.userName")
-                alert($data.userName)
                 sessionStorage.setItem("userNameForKovka", $data.userName);
                 location.href = "authorizedUser.html";
             });
-        /*
-           Данные: {"jwt":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzEsImlhdCI6MTY4NTQ3MDU3MywiZXhwIjoxNjg4MDYyNTczfQ.BcL2hdNLVgaIOtdGt-SLnVe5xMHIyCum-w8gvYp27UM","user":{"id":31,"username":"NaN","email":"oleggol95@gmail.com","provider":"local","confirmed":true,"blocked":false,"createdAt":"2023-05-30T18:16:13.358Z","updatedAt":"2023-05-30T18:16:13.358Z"}}
-           Состояние: success
-         */
     })
 
     $('#submit-button-message').on("click", function (e) {
@@ -34,15 +26,9 @@ $(document).ready(function () {
         var $data = {};
         var $token = sessionStorage.getItem("bearerTokenForUser")
         var $user = sessionStorage.getItem("userNameForKovka")
-        alert("$token")
-        alert($token)
-        alert("$user")
-        alert($user)
         $('#myMessageForm').find('input, textearea, select').each(function () {
             $data[this.name] = $(this).val();
         });
-        console.log("$data");
-        console.log($data);
         $.ajax({
             type: 'POST',
             url: "http://localhost:1337/api/messages",
@@ -55,33 +41,14 @@ $(document).ready(function () {
             headers: {
                 "authorization": $token,
             }
-            //OR
-            //beforeSend: function(xhr) {
-            //  xhr.setRequestHeader("My-First-Header", "first value");
-            //  xhr.setRequestHeader("My-Second-Header", "second value");
-            //}
         }).done(function (data) {
             alert(data);
         });
-        /*$.post("http://localhost:1337/api/auth/local/register",
-            {
-                "username": $data.user-name,
-                "email": $data.email,
-                "password": $data.password
-            },
-            function (data, status) {
-                console.log("Данные: " + JSON.stringify(data) + "\nСостояние: " + status);
-                localStorage.setItem("bearerTokenForUser", `Bearer ${data.jwt}`);
-                location.href = "authorizedUser.html";
-            });*/
-        /*
-           Данные: {"jwt":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzEsImlhdCI6MTY4NTQ3MDU3MywiZXhwIjoxNjg4MDYyNTczfQ.BcL2hdNLVgaIOtdGt-SLnVe5xMHIyCum-w8gvYp27UM","user":{"id":31,"username":"NaN","email":"oleggol95@gmail.com","provider":"local","confirmed":true,"blocked":false,"createdAt":"2023-05-30T18:16:13.358Z","updatedAt":"2023-05-30T18:16:13.358Z"}}
-           Состояние: success
-         */
     })
 
     var $messagesBlock = $("#messagesBlockWrapper");
     if($messagesBlock.is("#messagesBlockWrapper")){
+        var $token = sessionStorage.getItem("bearerTokenForUser")
         $.ajax({
             type: 'GET',
             url: "http://localhost:1337/api/messages",
