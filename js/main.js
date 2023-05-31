b5f5d5105267ced999e8e3262fd8eb7401f25c2a
olegJS95 <Oleg.Holoshchapov@filiatix.com>
2023-05-31 12:41:58 +0300

    project in progress

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     var $data = {};
     $('#submit-button-registration').on("click", function (e) {
-        e.preventDefault()
+        e.preventDefault();
         $('#myForm').find('input, textearea, select').each(function () {
             $data[this.name] = $(this).val();
         });
-        console.log("$data")
-        console.log($data)
+        console.log("$data");
+        console.log($data);
         $.post("http://localhost:1337/api/auth/local/register",
             {
                 "username": $data.user-name,
@@ -14,7 +14,9 @@ $(document).ready(function () {
                 "password": $data.password
             },
             function (data, status) {
-                alert("Данные: " + JSON.stringify(data) + "\nСостояние: " + status);
+                console.log("Данные: " + JSON.stringify(data) + "\nСостояние: " + status);
+                localStorage.setItem("bearerTokenForUser", `Bearer ${data.jwt}`);
+                location.href = "authorizedUser.html";
             });
         /*
            Данные: {"jwt":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzEsImlhdCI6MTY4NTQ3MDU3MywiZXhwIjoxNjg4MDYyNTczfQ.BcL2hdNLVgaIOtdGt-SLnVe5xMHIyCum-w8gvYp27UM","user":{"id":31,"username":"NaN","email":"oleggol95@gmail.com","provider":"local","confirmed":true,"blocked":false,"createdAt":"2023-05-30T18:16:13.358Z","updatedAt":"2023-05-30T18:16:13.358Z"}}
